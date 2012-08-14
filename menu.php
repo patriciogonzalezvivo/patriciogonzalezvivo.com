88adca94b254dde58f1b45a5cc1368b3b0df4aee
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-08-14 10:50:26 -0400

    other stuff

---
+++ menu.php
@@ -0,0 +1,17 @@
+<?php
+// main menu
+
+echo '<header>
+			<div id="menu">			
+				<a id="menu_logo" href="index.php"><img src="http://patriciogonzalezvivo.com/images/menu_logo.png" alt="Tree Logo" /></a> 
+				<nav id="menu_text">
+					<ul>
+						<li class="menu_item"><a href="http://patriciogonzalezvivo.com/about.php">About</a></li>
+						<li class="menu_item"><a href="http://patriciogonzalezvivo.com/index.php">Installations</a></li>
+						<li class="menu_item"><a href="http://patriciogonzalezvivo.com/apps.php">Apps</a></li>
+						<li class="menu_item"><a href="http://www.patriciogonzalezvivo.com/blog/">Blog</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>';
+?>
---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<?php
+echo '<section id="sidebar">
+		<article class="feeds">
+			<p><a href="http://twitter.com/patriciogv" target="_blank"><strong>@PatricioGV</strong></a>:</p>
+			<div id="twitter"></div>
+		</article>
+		<article class="feeds">
+			<p>Check out my latest work on <a href="http://github.com/patriciogv" target="_blank"><strong>GitHub</strong></a>:</p>
+			<div id="github"></div>
+		</article>
+	</section >';
+?>
