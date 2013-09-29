e74629ff5b5b74a5061a7af188a1a2e6f753e95a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-09-29 09:50:07 -0400

    Twitter wiget

--- menu.php
+++ menu.php
@@ -7,10 +7,10 @@ echo '<header>
 				<nav id="menu_text">
 					<ul>
 						<li class="menu_item"><a href="/about.php"> About Me</a></li>
-						<li class="menu_item"><a href="/index.php"> Interactive Instalations </a></li>
+						<li class="menu_item"><a href="/index.php"> Instalations </a></li>
 						<li class="menu_item"><a href="/code-poems.php"> Code Poems </a></li>
-						<li class="menu_item"><a href="/tools.php"> Creative Tools </a></li>
-						<li class="menu_item"><a href="/addons.php"> OpenFrameworks Addons </a></li>
+						<li class="menu_item"><a href="/tools.php"> Tools </a></li>
+						<li class="menu_item"><a href="/addons.php"> OF Addons </a></li>
 						<li class="menu_item"><a href="/teaching.php"> Teaching </a></li>
 					</ul>
 				</nav>
--- sidebar.php
+++ sidebar.php
@@ -1,22 +1,27 @@
 <?php
 echo '<section id="sidebar">
+		
+		<a class="twitter-timeline" href="https://twitter.com/patriciogv" data-widget-id="384308258309033984">Tweets by @patriciogv</a>
+	</section >';
+?>
+<!--
 
+		<article class="feeds">
+			
+			<h2><a href="http://twitter.com/patriciogv" target="_blank">Twitter</a></h2>
+			
+			<div id="twitter"></div>
+			<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/follow_button.1345016233.html#_=1345040039780&amp;id=twitter-widget-2&amp;lang=en&amp;screen_name=patriciogv&amp;show_count=true&amp;show_screen_name=true&amp;size=m" class="twitter-follow-button" style="width: 245px; height: 20px; " title="Twitter Follow Button"></iframe>
+			
+		</article>
 		
 		<article class="feeds">
 			<h2><a href="http://patriciogonzalezvivo.com/blog" target="_blank">Blog</a></h2>
 			<div id="wordpress"></div>
 		</article>
+		
 		<article class="feeds">
 			<h2><a href="http://github.com/patriciogonzalezvivo" target="_blank">GitHub</a></h2>
 			<div id="github"></div>
 		</article>
-	</section >';
-?>
-<!--
-<article class="feeds">
-			<h2><a href="http://twitter.com/patriciogv" target="_blank">Twitter</a></h2>
-			<div id="twitter"></div>
-			<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/follow_button.1345016233.html#_=1345040039780&amp;id=twitter-widget-2&amp;lang=en&amp;screen_name=patriciogv&amp;show_count=true&amp;show_screen_name=true&amp;size=m" class="twitter-follow-button" style="width: 245px; height: 20px; " title="Twitter Follow Button"></iframe>
-		</article>
-
 -->
