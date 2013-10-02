fe97867915d83a700169a26b2de30666fbca838c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-10-02 19:19:59 -0400

    cleaning

--- 2012/efectomariposa/index.php
+++ 2012/efectomariposa/index.php
@@ -4,8 +4,9 @@
 
 	<!-- CONTENT -->
 	<section class="content">
+	
 		<div id="myslides">
-			<img class="roundPhoto" src="01.jpg" alt="slide" />
+			<img class="roundPhoto" src="01.jpg" alt="slide"/>
 			<img class="roundPhoto" src="02.jpg" alt="slide"/>
 			<img class="roundPhoto" src="03.jpg" alt="slide"/>
 			<img class="roundPhoto" src="04.jpg" alt="slide"/>
@@ -14,12 +15,15 @@
 			<img class="roundPhoto" src="07.jpg" alt="slide"/>
 			<img class="roundPhoto" src="08.jpg" alt="slide"/>
 		</div>
+		
 		<article class="cita">
 			<p><i>"A butterfly can flutter its wings over a flower in China and cause a hurricane in the Caribbean"</i></p>
 		</article>
+		
 		<article>
 			<p>This is a remade of <a href="http://patriciogonzalezvivo.com/2011/efectomariposa/" target="_blank">Efecto Mariposa</a> that was re-designed site specific for FILE 2012 Sao Paulo Festival.</p>
 		</article>
+		
 		<div>
 			<a href="http://filefestival.org/"><img src="sponsor.jpg" alt="sponsor"/></a>
 		</div>
