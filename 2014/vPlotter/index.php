3af6659e29a09248d9df29c9d72e4b52f7cd91d4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-04-27 15:00:55 -0400

    reading directly from git

--- 2014/vPlotter/index.php
+++ 2014/vPlotter/index.php
@@ -12,21 +12,21 @@
 			define('GITHUB_REPO', 'vPlotter');
 			define('GITHUB_BRANCH', 'master');
 
-			function get_raw_url($path) {
-				return 'https://raw.github.com/'.GITHUB_USER.'/'.GITHUB_REPO.'/'.GITHUB_BRANCH.'/'.$path;
-			}
-
-			function cache_file($url, $path) {
-				if (file_exists($path)) {
-					unlink($path);
-				}
-				file_put_contents($path, file_get_contents($url));
-			}
-			cache_file(get_raw_url('README.md'), __DIR__ . '/about.md');
+			// function get_raw_url($path) {
+			// 	return 'https://raw.github.com/'.GITHUB_USER.'/'.GITHUB_REPO.'/'.GITHUB_BRANCH.'/'.$path;
+			// }
+			//
+			// function cache_file($url, $path) {
+			// 	if (file_exists($path)) {
+			// 		unlink($path);
+			// 	}
+			// 	file_put_contents($path, file_get_contents($url));
+			// }
+			// cache_file(get_raw_url('README.md'), __DIR__ . '/about.md');
 
 			$Parsedown = new Parsedown();
-			echo $Parsedown->text(file_get_contents( __DIR__. '/about.md' ));
-
+			// echo $Parsedown->text(file_get_contents( __DIR__. '/about.md' ));
+			echo $Parsedown->text(file_get_contents( 'https://raw.github.com/'.GITHUB_USER.'/'.GITHUB_REPO.'/'.GITHUB_BRANCH.'/README.md'));
 		?>
 
 	</section>
