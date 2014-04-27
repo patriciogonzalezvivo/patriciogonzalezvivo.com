387893f0bc9cbdb8a5879fa9011616af1636cde4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-04-27 18:19:33 -0400

    cleaning

--- 2011/ofxfx/index.php
+++ 2011/ofxfx/index.php
@@ -4,18 +4,14 @@
 
 	<!-- CONTENT -->
 	<section class="content">
-		<article>
-			<p>OpenFrameworks addon for super easy and flexible GLSL Shader integrations. This begins while I was working on <a href="http://patriciogonzalezvivo.com/2011/efectomariposa/">Efecto Mariposa</a> and I need some extra GPU processing power in order to make fast simulation of complex ecosystems</p>
-			<p>Source at: <a href="https://github.com/patriciogonzalezvivo/ofxFX" target="_blank"> https://github.com/patriciogonzalezvivo/ofxFX</a></p>
-		</article>
-		<article>
-			<iframe src="http://player.vimeo.com/video/35230297" width="575" height="453" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-			<iframe src="http://player.vimeo.com/video/34304485" width="575" height="431" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-			<iframe src="http://player.vimeo.com/video/30684308" width="575" height="429" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-			<iframe src="http://player.vimeo.com/video/29887776" width="575" height="431" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-			<iframe src="http://player.vimeo.com/video/29896245" width="575" height="431" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-			<iframe src="http://player.vimeo.com/video/29894099" width="575" height="431" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</article>
+
+		<?php
+			include("../../parsedown/Parsedown.php");
+			define('GITHUB_REPO', 'ofxFX');
+			$Parsedown = new Parsedown();
+			echo $Parsedown->text(file_get_contents( 'https://raw.github.com/patriciogonzalezvivo/'.GITHUB_REPO.'/master/README.md'));
+		?>
+
 	</section>
-	
+
 <?php include("../../footer.php"); ?>
