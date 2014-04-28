b4788187369d1080f97b38c46b02dc853bc30fb3
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-04-28 08:56:15 -0400

    ofxPulseSensor

--- 2013/ofxpulsesensor/index.php
+++ 2013/ofxpulsesensor/index.php
@@ -4,25 +4,14 @@
 
 	<!-- CONTENT -->
 	<section class="content">
-		
-		<a href="https://github.com/patriciogonzalezvivo/ofxPulseSensor" target="_blank">
-		<div id="myslides">
-			<img class="roundPhoto" src="01.jpg" alt="slide"/>
-			<img class="roundPhoto" src="02.jpg" alt="slide"/>
-			<img class="roundPhoto" src="03.jpg" alt="slide"/>
-		</div>
-		</a>
-		
-		<article>
-			<p> <a href="http://www.openframeworks.cc/" target="_blank">OpenFrameworks</a> addon for the <a href="http://pulsesensor.myshopify.com/" target="_blank">PulseSensor</a> device developed by <a href="http://www.joelmurphy.net/" target="_blank">Joel Murphy</a> and <a href="http://www.mybeatingheart.com/yury/" target="_blank">Yury Gitman</a>.</p>
-			<p>This addon helps you get the heartbeat (BPM and IBI) and use it in your openFrameworks project using the GPIO's of your <a href="http://www.raspberrypi.org/" target="_blank">RaspberryPi</a> or by using an <a href="http://www.arduino.cc/" target="_blank">Arduino</a> to send the signal by Serial connection.</p>
-			<p>Check out the code in my repo: <a href="https://github.com/patriciogonzalezvivo/ofxPulseSensor" target="_blank">https://github.com/patriciogonzalezvivo/ofxPulseSensor</a></p>
-		</article>
-		
-		<div class="video-container">
-			<iframe src="http://player.vimeo.com/video/77236762" width="575" height="359" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> 
-		</div>
-		
+
+		<?php
+			include("../../parsedown/Parsedown.php");
+			define('GITHUB_REPO', 'ofxPulseSensor');
+			$Parsedown = new Parsedown();
+			echo $Parsedown->text(file_get_contents( 'https://raw.github.com/patriciogonzalezvivo/'.GITHUB_REPO.'/master/README.md'));
+		?>
+
 	</section>
-	
+
 <?php include("../../footer.php"); ?>
