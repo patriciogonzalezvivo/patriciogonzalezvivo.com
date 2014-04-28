c4543e16cc17d29a19df401a9b8d321365ed08ff
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-04-27 23:19:57 -0400

    ofxComposer

--- 2012/ofxcomposer/index.php
+++ 2012/ofxcomposer/index.php
@@ -4,13 +4,14 @@
 
 	<!-- CONTENT -->
 	<section class="content">
-		<div class="video-container">
-			<iframe src="http://player.vimeo.com/video/38858952?autoplay=1" width="575" height="359" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</div>
-		<article>
-			<p>This idea came up with James George at Detroit´s oF DevCon 2012. It consist on a set of connecting boxes that let you load and processes different type of data on the GPU. It´s designed to be flexible and visualy minimalist for quick editing on life performances. Also it's very handy for graphical debugging, editing GLSL Shaders on the fly, and as a ofxGui complement.</p>
-			<p>Check out the code at: <a href="http://github.com/patriciogonzalezvivo/ofxComposer" target="_blank">https://github.com/patriciogonzalezvivo/ofxComposer</a></p>
-		</article>
-    </section>
-	
+
+	<?php
+		include("../../parsedown/Parsedown.php");
+		define('GITHUB_REPO', 'ofxComposer');
+		$Parsedown = new Parsedown();
+		echo $Parsedown->text(file_get_contents( 'https://raw.github.com/patriciogonzalezvivo/'.GITHUB_REPO.'/master/README.md'));
+	?>
+
+  </section>
+
 <?php include("../../footer.php"); ?>
