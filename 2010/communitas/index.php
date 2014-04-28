76e03f912a2b34e282d2dc586b0778d4dedda166
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-04-27 23:07:40 -0400

    kinectCoreVision comunitas

--- 2010/communitas/index.php
+++ 2010/communitas/index.php
@@ -5,31 +5,14 @@
 
 	<!-- CONTENT -->
 	<section class="content">
-		<a href="https://vimeo.com/15171352" target="_blank">
-		<div id="myslides">
-			<img class="roundPhoto" src="01.jpg" alt="slide"/>
-			<img class="roundPhoto" src="02.jpg" alt="slide"/>
-			<img class="roundPhoto" src="03.jpg" alt="slide"/>
-			<img class="roundPhoto" src="04.jpg" alt="slide"/>
-			<img class="roundPhoto" src="05.jpg" alt="slide"/>
-			<img class="roundPhoto" src="06.jpg" alt="slide"/>
-			<img class="roundPhoto" src="07.jpg" alt="slide"/>
-			<img class="roundPhoto" src="08.jpg" alt="slide"/>
-			<img class="roundPhoto" src="09.jpg" alt="slide"/>
-			<img class="roundPhoto" src="10.jpg" alt="slide"/>
-		</div></a>
-		<article class="cita">
-			<p><i>“When we forget ourselves, we are the universe..”</i><br>Hakui</p>
-		</article>
-		<article>
-			<p><strong>Communitas</strong> is an interactive multiTouch table made using openFrameworks and TUIO protocol. It invites people to create collective drawings. Its goal is to allow people to explore the dynamic process of co-creation with an emphasis on the core value of respecting individual differences.</p>
-		</article>
-		<div class="video-container">
-			<iframe src="http://player.vimeo.com/video/15171352?autoplay=1" width="575" height="323" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</div>
-		<div>
-			<a href="http://www.espacioft.org.ar/"><img src="sponsor.jpg" alt="sponsor"/></a>
-		</div>
+
+		<?php
+			include("../../parsedown/Parsedown.php");
+			define('GITHUB_REPO', 'Communitas');
+			$Parsedown = new Parsedown();
+			echo $Parsedown->text(file_get_contents( 'https://raw.github.com/patriciogonzalezvivo/'.GITHUB_REPO.'/master/README.md'));
+		?>
+
 	</section>
-	
+
 <?php include("../../footer.php"); ?>
--- 2011/kinectcorevision/index.php
+++ 2011/kinectcorevision/index.php
@@ -4,16 +4,14 @@
 
 	<!-- CONTENT -->
 	<section class="content">
-		<div class="video-container">
-			<iframe class="roundPhoto" src="http://player.vimeo.com/video/20904879?autoplay=1" width="575" height="359" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</div>
-		<article>
-			<p><strong>KinectCoreVision</strong> Modd version of the well know track application CommunityCoreVision for Kinect and 3D cameras using OpenNI drivers.</p>
-			<p>This version of CCV was original designed for a light and portable setup of Communias multiTouch Table for IEATA´s 9th International Conference ( more info and videos about it ). But it was lot of potencial. Fell free to download the code and make your own improvements:</p>
-		</article>
-		<article>
-			<p>Download: <a href="http://github.com/patriciogonzalezvivo/KinectCoreVision" target="_blank">[Source] </a> <a href="https://dl.dropbox.com/u/335522/web/apps/KinectCoreVision-10.6.zip" target="_blank">[OSX Snow Leopard App]</a> <a href="http://dl.dropbox.com/u/335522/web/apps/KinectCoreVision.zip" target="_blank">[OSX Lion App]</a> <a href="http://dl.dropbox.com/u/335522/web/apps/KinectCoreVision-Win.zip" target="_blank">[WinXp Bin]</a> <a href="https://dl.dropbox.com/u/335522/web/apps/KinectCoreVision-win7.zip" target="_blank">[Win7 Bin]</a> <a href="http://dl.dropbox.com/u/335522/web/apps/KinectCoreVision-linux64.zip" target="_blank"> [Linux 64bit]</a><br/></p>
-		</article>
-    </section>
-	
+
+		<?php
+			include("../../parsedown/Parsedown.php");
+			define('GITHUB_REPO', 'KinectCoreVision');
+			$Parsedown = new Parsedown();
+			echo $Parsedown->text(file_get_contents( 'https://raw.github.com/patriciogonzalezvivo/'.GITHUB_REPO.'/master/README.md'));
+		?>
+
+	</section>
+
 <?php include("../../footer.php"); ?>
