8652f8574b54f053e3470487d3d8d41c1bc14e79
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-04-27 20:31:48 -0400

    voro

--- 2013/ofxvoro/index.php
+++ 2013/ofxvoro/index.php
@@ -4,19 +4,15 @@
 
 	<!-- CONTENT -->
 	<section class="content">
-		<div class="video-container">
-			<iframe src="http://player.vimeo.com/video/64520422" width="575" height="323" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</div>
-		
-		<article>
-			<p>Addon for openFrameworks that let you wrap <a href="http://math.lbl.gov/voro++/" target="_blank">Voro++</a> for making 3D Voronoi Tessalations</p>
-			<p>Check out the code at: <a href="https://github.com/patriciogonzalezvivo/ofxVoro" target="_blank">https://github.com/patriciogonzalezvivo/ofxVoro</a></p>
-		</article>
-		
-		<div class="video-container">
-			<iframe src="http://player.vimeo.com/video/64697511" width="575" height="323" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</div>
-		
+
+	<?php
+		include("../../parsedown/Parsedown.php");
+		define('GITHUB_REPO', 'ofxVoro');
+		$Parsedown = new Parsedown();
+		echo $Parsedown->text(file_get_contents( 'https://raw.github.com/patriciogonzalezvivo/'.GITHUB_REPO.'/master/README.md'));
+	?>
+
+
 	</section>
-	
+
 <?php include("../../footer.php"); ?>
