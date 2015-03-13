5850e153e01af1e8d33b87c5ca08173d9ef64d0f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-03-13 00:18:27 -0400

    menu

--- menu.php
+++ menu.php
@@ -7,7 +7,6 @@ echo '<header>
 				<nav id="menu_text">
 					<ul>
 						<li class="menu_item"><a href="/about.php"> Me </a></li>
-						<li class="menu_item"><a href="/inst.php"> Installations </a></li>
 						<li class="menu_item"><a href="/collab.php"> Collaborations </a></li>
 						<li class="menu_item"><a href="/tools.php"> Tools </a></li>
 						<li class="menu_item"><a href="/experiments.php"> Experiments </a></li>
