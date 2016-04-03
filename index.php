8e7408aceaf3f8ea1f3b6dbfb8f3ab5ac35c5558
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-04-03 14:54:23 -0400

    no more ISS project

--- index.php
+++ index.php
@@ -2,59 +2,55 @@
 <?php include("menu.php");?>
 <?php include("sidebar.php");?>
 
-	<section class="content">
-		<article class="item">
-			<a href="2016/openFrame/"><img class="roundPhotoTh" src="2016/openFrame/thumb1.png" alt="openframe"/></a>
-			<p>2016 shaders artwork for <strong>openFrame.io</strong></p>
-		</article>
-		<article class="item">
-			<a href="http://patriciogonzalezvivo.github.io/LineOfSight"><img class="roundPhotoTh" src="2015/LineOfSight/thumb.gif" alt="LineOfSight"/></a>
-			<p>2015 <strong>LineOfSight</strong>: find and track satellites</p>
-		</article>
-		<article class="item">
-			<a href="http://patriciogonzalezvivo.github.io/ISS/"><img class="roundPhotoTh" src="2015/ISS/thumb.gif" alt="OSM from the ISS"/></a>
-			<p>2015 <strong>ISS</strong>: zoom and see the view of openStreetMap from the International Space Station</p>
-		</article>
-		<article class="item">
-			<a href="http://patriciogonzalezvivo.github.io/RandomCity/"><img class="roundPhotoTh" src="2015/RandomCity/thumb.gif" alt="RandomCity"/></a>
-			<p>2015 <strong>RandomCity</strong>: though the streams are chaotic, they coalesce into a digital harmony</p>
-		</article>
-		<article class="item">
-			<a href="http://thebookofshaders.com/"><img class="roundPhotoTh" src="2015/thebookofshaders/thumb.jpg" alt="TheBookOfShaders"/></a>
-			<p>2015 <strong>The Book of Shaders</strong>: Who said #shaders are a dark art? </p>
-		</article>
-		<article class="item">
-			<a href="2015/tangram-sandbox/"><img class="roundPhotoTh" src="2015/tangram-sandbox/thumb.png" alt="Tangram-Sandbox"/></a>
-			<p>2015 <strong>Tangram Sandbox</strong>: series of experimental styles for maps based on GLSL shaders</p>
-		</article>
-		<article class="item">
-			<a href="2014/lidar/"><img class="roundPhotoTh" src="2014/lidar/thumb.gif" alt="Lidar"/></a>
-			<p>2014 <strong>crowdsourcing Point Clouds </strong>for 3D mapping.</p>
-		</article>
-		<article class="item">
-			<a href="2014/skylines/"><img class="roundPhotoTh" src="2014/skylines/thumb.png" alt="SKYLINES"/></a>
-			<p>2014 <strong>SKYLINES</strong>: three projects about awareness and tools for seeing</p>
-		</article>
-		<article class="item">
-			<a href="2014/pointcloudcity/"><img class="roundPhotoTh" src="2014/pointcloudcity/thumb.gif" alt="PointCloudCity"/></a>
-			<p>2014 <strong>Point Cloud City</strong>: private data landscapes of public places</p>
-		</article>
-		<article class="item">
-			<a href="2013/clouds/"><img class="roundPhotoTh" src="2013/clouds/thumb.jpg" alt="clouds"/></a>
-			<p>2013 <strong>CLOUDS Documentary</strong>: collaboration of poetic Visual Systems</p>
-		</article>
-		<article class="item">
-			<a href="2012/efectomariposa/"><img class="roundPhotoTh" src="2012/efectomariposa/thumb.jpg" alt="Efecto Mariposa"/></a>
-			<p>2012 <strong>Efecto Mariposa 3.0</strong>: interactive installation at FILE, Sao Paulo, Brazil</p>
-		</article>
-		<article class="item">
-			<a href="2011/efectomariposa/"><img class="roundPhotoTh" src="2011/efectomariposa/thumb.jpg" alt="Efecto Mariposa"/></a>
-			<p>2011 <strong>Efecto Mariposa 1.0</strong>: interactive installation at MediaLab 2011, CCEBA, Buenos Aires, Argentina</p>
-		</article>
-		<article class="item">
-			<a href="2010/communitas/"><img class="roundPhotoTh" src="2010/communitas/thumb.jpg" alt="Communitas"/></a>
-			<p>2010 <strong>Communitas</strong>: interactive installation at Interactivos 2010, Espacio Fundación Telefónica, Buenos Aires, Argentina</p>
-		</article>
-	</section>
+    <section class="content">
+        <article class="item">
+            <a href="2016/openFrame/"><img class="roundPhotoTh" src="2016/openFrame/thumb1.png" alt="openframe"/></a>
+            <p>2016 shaders artwork for <strong>openFrame.io</strong></p>
+        </article>
+        <article class="item">
+            <a href="http://patriciogonzalezvivo.github.io/LineOfSight"><img class="roundPhotoTh" src="2015/LineOfSight/thumb.gif" alt="LineOfSight"/></a>
+            <p>2015 <strong>LineOfSight</strong>: find and track satellites</p>
+        </article>
+        <article class="item">
+            <a href="http://patriciogonzalezvivo.github.io/RandomCity/"><img class="roundPhotoTh" src="2015/RandomCity/thumb.gif" alt="RandomCity"/></a>
+            <p>2015 <strong>RandomCity</strong>: though the streams are chaotic, they coalesce into a digital harmony</p>
+        </article>
+        <article class="item">
+            <a href="http://thebookofshaders.com/"><img class="roundPhotoTh" src="2015/thebookofshaders/thumb.jpg" alt="TheBookOfShaders"/></a>
+            <p>2015 <strong>The Book of Shaders</strong>: Who said #shaders are a dark art? </p>
+        </article>
+        <article class="item">
+            <a href="2015/tangram-sandbox/"><img class="roundPhotoTh" src="2015/tangram-sandbox/thumb.png" alt="Tangram-Sandbox"/></a>
+            <p>2015 <strong>Tangram Sandbox</strong>: series of experimental styles for maps based on GLSL shaders</p>
+        </article>
+        <article class="item">
+            <a href="2014/lidar/"><img class="roundPhotoTh" src="2014/lidar/thumb.gif" alt="Lidar"/></a>
+            <p>2014 <strong>crowdsourcing Point Clouds </strong>for 3D mapping.</p>
+        </article>
+        <article class="item">
+            <a href="2014/skylines/"><img class="roundPhotoTh" src="2014/skylines/thumb.png" alt="SKYLINES"/></a>
+            <p>2014 <strong>SKYLINES</strong>: three projects about awareness and tools for seeing</p>
+        </article>
+        <article class="item">
+            <a href="2014/pointcloudcity/"><img class="roundPhotoTh" src="2014/pointcloudcity/thumb.gif" alt="PointCloudCity"/></a>
+            <p>2014 <strong>Point Cloud City</strong>: private data landscapes of public places</p>
+        </article>
+        <article class="item">
+            <a href="2013/clouds/"><img class="roundPhotoTh" src="2013/clouds/thumb.jpg" alt="clouds"/></a>
+            <p>2013 <strong>CLOUDS Documentary</strong>: collaboration of poetic Visual Systems</p>
+        </article>
+        <article class="item">
+            <a href="2012/efectomariposa/"><img class="roundPhotoTh" src="2012/efectomariposa/thumb.jpg" alt="Efecto Mariposa"/></a>
+            <p>2012 <strong>Efecto Mariposa 3.0</strong>: interactive installation at FILE, Sao Paulo, Brazil</p>
+        </article>
+        <article class="item">
+            <a href="2011/efectomariposa/"><img class="roundPhotoTh" src="2011/efectomariposa/thumb.jpg" alt="Efecto Mariposa"/></a>
+            <p>2011 <strong>Efecto Mariposa 1.0</strong>: interactive installation at MediaLab 2011, CCEBA, Buenos Aires, Argentina</p>
+        </article>
+        <article class="item">
+            <a href="2010/communitas/"><img class="roundPhotoTh" src="2010/communitas/thumb.jpg" alt="Communitas"/></a>
+            <p>2010 <strong>Communitas</strong>: interactive installation at Interactivos 2010, Espacio Fundación Telefónica, Buenos Aires, Argentina</p>
+        </article>
+    </section>
 
 <?php include("footer.php"); ?>
