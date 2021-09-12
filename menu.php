303fd8e8bb630e24ac42132cdf51fe171a4b8a62
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-09-12 07:11:31 -0400

    cleaning

--- menu.php
+++ menu.php
@@ -6,10 +6,11 @@ echo '<header>
 				<a id="menu_logo" href="/index.php"><img id="menu_logo_img" src="/images/menu_logo.png" alt="Tree Logo" /></a>
 				<nav id="menu_text">
 					<ul>
-						<li class="menu_item"><a href="/about.php"> Me </a></li>
+						<li class="menu_item"><a href="/about.php"> Bio </a></li>
+						<li class="menu_item"><a href="/index.php"> Works </a></li>
 						<li class="menu_item"><a href="/tools.php"> Tools </a></li>
-						<li class="menu_item"><a href="/teaching.php"> Teaching </a></li>
-						<li class="menu_item"><a href="/collab.php"> Collab</a></li>
+						<li class="menu_item"><a href="/teaching.php"> Ed. </a></li>
+						<li class="menu_item"><a href="/collab.php"> Collab </a></li>
 						<li class="menu_item"><a href="https://shop.patriciogonzalezvivo.com/" target="_blank"> Shop </a></li>
 					</ul>
 					
@@ -17,13 +18,14 @@ echo '<header>
 					<a href="https://twitter.com/intent/follow?screen_name=patriciogv"><img src="/images/icons/twitter.png" width="32" class="icon"/></a>
 					<a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/instagram.png" width="32" class="icon"/></a>
 					<a href="https://www.hicetnunc.xyz/patriciogonzalezvivo/creations" target="_blank"><img src="/images/icons/hen.png" width="32" class="icon"/></a>
-					<a href="https://github.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/github.png" width="30" class="icon"/></a>
 					</section >
 					
-				</nav>
-			</div>
+					</nav>
+					</div>
+					
+					</header>';
+					
+					
+					?>
 
-		</header>';
-
-		
-?>
+<!-- <a href="https://github.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/github.png" width="30" class="icon"/></a> -->
