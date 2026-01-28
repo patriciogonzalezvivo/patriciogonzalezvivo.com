c486be5375158e0d837a0b0b64bae935ec059f6a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-01-28 06:55:13 -0500

    clouds

--- index.php
+++ index.php
@@ -96,10 +96,10 @@
             <a href="2014/pointcloudcity/"><img class="roundPhotoTh" src="2014/pointcloudcity/thumb.gif" alt="PointCloudCity"/></a>
             <p>2014 <strong>Point Cloud City</strong>: private data landscapes of public places</p>
         </article>
-        <!-- <article class="item">
-            <a href="2013/clouds/"><img class="roundPhotoTh" src="2013/clouds/thumb.jpg" alt="clouds"/></a>
+        <article class="item">
+            <a href="https://cloudsdocumentary.com"><img class="roundPhotoTh" src="2013/clouds/thumb.jpg" alt="clouds"/></a>
             <p>2013 <strong>CLOUDS Documentary</strong>: collaboration of poetic Visual Systems</p>
-        </article> -->
+        </article>
 		<!-- <article class="item">
             <a href="https://vimeo.com/54093238" target="_blank"><img class="roundPhotoTh" src="2012/codemology/thumb.jpg" alt="CodeMology"/></a>
 			<p>2012 <strong>Codemology</strong>: explorations between code and alquemy</p>
