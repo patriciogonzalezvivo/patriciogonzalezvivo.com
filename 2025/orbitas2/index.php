1d4b5c7b462d9cf965887da6203481c707c52d88
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-02-22 12:13:54 -0500

    orbitas

---
+++ 2025/orbitas2/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Estrellas</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="thumb.gif" />
+        <meta property="og:title" content="Orbitas" />
+        <meta property='og:description' content=''/>
+        <meta property='og:image:width' content='512'/>
+        <meta property='og:image:height' content='512'/>
+        <meta property='og:site_name' content='Patricio Gonzalez Vivo'/>
+
+        <meta name="description" content="Orbitas, a living cosmic choreography" />
+        <meta name="keywords" content="Orbitas, Patricio Gonzalez Vivo" />
+        <meta name="author" content="Patricio Gonzalez Vivo" />
+        
+        <link href="../../ico.gif" rel="shortcut icon"  />
+        <link href="../../css/style.css" rel="stylesheet" />
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,200italic,300italic,400italic" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+        <script>
+            (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+            ga("create", "UA-18824436-1", "patriciogonzalezvivo.com");
+            ga("send", "pageview");
+        </script>
+
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W5MR6SK1EZ');
+        </script>
+    </head>
+    <body class="windowed-mode">
+        
+        <div id="main-wrapper">
+            <?php include("../../menu.php");?>
+        </div>
+
+        <div id="wrapper" class="windowed">
+            <img id="frame-back" class="frame" src="frame_background.png" alt="">
+                <canvas class='emscripten' id='canvas' oncontextmenu='event.preventDefault()' tabindex=-1></canvas>
+            <img id="frame-front" class="frame" src="frame_refleccion.png" alt="">
+        </div>
+        <div id="ui">
+            <a id="frm-btn" href="https://frm.fm/a/patricio_gonzalez_vivo/orbitas" target="_blank">
+                <img src="frm_logo.png" alt="FRM Logo">
+            </a>
+            <button id="resize-btn">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg>
+            </button>
+        </div>
+        <div id="info">
+            <h1>Orbitas v2</h1>
+            <h2>by <a href="https://patriciogonzalezvivo.com">Patricio Gonzalez Vivo</a></h2>
+            <p>Orbitas is a living cosmogram, a real-time simulation where planets and moons inscribe their movements into space. As the camera drifts through the solar system, luminous trails unfold, tracing celestial paths accumulated since the beginning of the year, turning motion into memory.</p>
+            <p>Earth’s tilt and passage through the months align with distant stars and zodiac constellations, while the Moon cycles through its phases, breathing rhythm into the work. Time expands, scale dissolves, and the familiar calendar gives way to a cosmic one.</p>
+            <p>Both instrument and invocation, Orbitas invites contemplation of the great celestial dance, a quiet reminder of our place within the vast, slow choreography of the Sun and its orbiting worlds.</p>
+        </div>
+        <div id="longer-info">
+            <p>Exibitions:</p>
+            <ul>
+                <p>2025 - Solar Biennale, at <a href="https://www.instagram.com/mudaclausanne/">Museum of Contemporary Design</a>, Switzerland</p>
+                <p>2018 - <a href="https://frm.fm/a/patricio_gonzalez_vivo/orbitas">Framed</a></p>
+            </ul>
+        </div>
+        <wasm-loader></wasm-loader>
+
+        <footer>
+            <p>© Patricio Gonzalez Vivo 2026</p>
+        </footer>
+    </body>
+    <script type="module" src="main.js"></script>
+</html>
