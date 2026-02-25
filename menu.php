c1ff9184c4494174d890ec1e5d653a5fdd4153ee
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-02-25 17:43:57 -0500

    weaver

--- menu.php
+++ menu.php
@@ -11,9 +11,9 @@
 				</ul>
 					
 				<section id="sidebar">
-					<a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/instagram.png" width="32" class="icon" style="position: relative;left: 5px;"/></a>
-					<a href="https://twitter.com/intent/follow?screen_name=patriciogv"><img src="/images/icons/twitter.png" width="34" class="icon"/></a>
-					<a href="https://bsky.app/profile/patricio.io"><img src="/images/icons/bsky.png" width="24" class="icon" style="top: -4px;position: relative;"/></a>
+					<a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank" target="_blank"><img src="/images/icons/instagram.png" width="32" class="icon" style="position: relative;left: 5px;"/></a>
+					<a href="https://twitter.com/intent/follow?screen_name=patriciogv" target="_blank"><img src="/images/icons/twitter.png" width="34" class="icon"/></a>
+					<a href="https://bsky.app/profile/patricio.io" target="_blank"><img src="/images/icons/bsky.png" width="24" class="icon" style="top: -4px;position: relative;"/></a>
 				</section >
 			</nav>
 		</div>	
