1af33cb6e6acfc2a752ab40168d237aa4836d0ce
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-02-26 07:47:47 -0500

    effecto mariposa

--- 2011/efectomariposa/index.php
+++ 2011/efectomariposa/index.php
@@ -2,75 +2,74 @@
 <?php include("../../menu.php");?>
 <?php include("../../sidebar.php");?>
 
-    <!-- CONTENT -->
-
-        <img class="roundPhoto" src="puyehue-04.jpg" style="width: 100%"/>
-
-        <article>
-            <p>On Saturday June 4, 2011, after decades of inactivity, Puyehue Volcano ejected a plume of ash 10 kilometers high and 5 wide. This led to a natural disaster with strong environmental and financial impacts in the region. At the same time, the ash killing thousands of living beings ensures the fertility of the region for years to come.</p>
-        </article>
-        
-        <article>
-            <p>The same ash is the raw material of this work. EfectoMariposa (Spanish for "Butterfly Effect") is an interactive installation that provides the opportunity to explore destruction and creation, which are combined in an infinite fractal dance, demonstrating the ability of life to cut through the face of adversity.</p>
-            
-            <img class="roundPhoto" src="004.gif" style="width: 100%"/>
-            <p>This is done by simulating an ecosystem over the surface of an ash heap. The user can modify with his own hands the "topology"and the "atmosphere" of this virtual world, unleashing all sorts of climatic and topographic changes that directly influence the conditions of life.</p>
-
-            <img class="roundPhoto" src="001.gif" style="width: 100%"/>
-            <p>The invitation is to alternate the perception between the micro and the macro scale to discover the complexity and richness of the world around us, as well as to realize our participation and responsibility in this whole.</p>
-            <img class="roundPhoto" src="003.gif" style="width: 100%"/>
-        </article>
-
-        <div class="video-container">
-            <!-- <iframe src="http://player.vimeo.com/video/32321634?autoplay=1" width="575" height="323" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
-                <iframe title="vimeo-player" src="https://player.vimeo.com/video/32321634?h=654b440c04" width="640" height="360" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"   allowfullscreen></iframe>
-        </div>
-        
-        <section class="content">
         <a href="https://vimeo.com/32321634" target="_blank">
-        <div id="myslides">
-            <img class="roundPhoto" src="01.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="02.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="03.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="04.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="05.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="06.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="07.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="08.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="09.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="10.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="11.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="12.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="13.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="14.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="15.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="16.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="17.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="18.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="19.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="20.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="21.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="22.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="23.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="24.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="25.jpg" style="width: 100%" alt="slide"/>
-            <img class="roundPhoto" src="26.jpg" style="width: 100%" alt="slide"/>
-        </div></a>        
-        
-        <article>
-            <p>Spacial thanks to:</p>
-            <p>- CCEBA coordinators and jury: Emiliano Causa, Matias Romero Costas and Federico Joselevich</p>
-            <p>- Volcanic Ash: Andres Rey and Juan Rey</p>
-            <p>- Ironworks: Juan Manuel Toconas</p>
-            <p>- Model of the rock: Fabian Nonino</p>
-            <p>- Teaching of openGL and Shader Lenguajes: Fabricio Costa</p>
-            <p>- Logo and app icons Design: Jovana de Obaldia</P>
-            <p>- Photography and Video: Joaquin Aras, Agustin Anzorena y Tomas Rawski</p>
-        </article>
-        <div>
-            <a href="http://www.cceba.org.ar/"><img src="sponsor.jpg" alt="sponsor"/></a>
-            <a href="http://www.cceba.org.ar/"><img src="sponsor_2.jpg" alt="sponsor"/></a>
+        <div id="myslides" class="roundPhoto" style="">
+            <img class="roundPhoto" src="puyehue-04.jpg" style="width: 100%; height: 640px; object-fit: cover;"/>
+            <img class="roundPhoto" src="01.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="02.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="03.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="04.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="05.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="06.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="07.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="08.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="09.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="10.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="11.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="12.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="13.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="14.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="15.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="16.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="17.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="18.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="19.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="20.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="21.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="22.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="23.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="24.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="25.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            <img class="roundPhoto" src="26.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+        </div></a>  
+
+        <div id="longer-info">
+            <article>
+                <p>On Saturday June 4, 2011, after decades of inactivity, Puyehue Volcano ejected a plume of ash 10 kilometers high and 5 wide. This led to a natural disaster with strong environmental and financial impacts in the region. At the same time, the ash killing thousands of living beings ensures the fertility of the region for years to come.</p>
+            </article>
+            
+            <article>
+                <p>The same ash is the raw material of this work. EfectoMariposa (Spanish for "Butterfly Effect") is an interactive installation that provides the opportunity to explore destruction and creation, which are combined in an infinite fractal dance, demonstrating the ability of life to cut through the face of adversity.</p>
+                
+                <img class="roundPhoto" src="004.gif" style="width: 100%"/>
+                <p>This is done by simulating an ecosystem over the surface of an ash heap. The user can modify with his own hands the "topology"and the "atmosphere" of this virtual world, unleashing all sorts of climatic and topographic changes that directly influence the conditions of life.</p>
+
+                <img class="roundPhoto" src="001.gif" style="width: 100%"/>
+                <p>The invitation is to alternate the perception between the micro and the macro scale to discover the complexity and richness of the world around us, as well as to realize our participation and responsibility in this whole.</p>
+                <img class="roundPhoto" src="003.gif" style="width: 100%"/>
+            </article>
+
+            <div class="video-container">
+                <!-- <iframe src="http://player.vimeo.com/video/32321634?autoplay=1" width="575" height="323" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> -->
+                    <iframe title="vimeo-player" src="https://player.vimeo.com/video/32321634?h=654b440c04" width="640" height="360" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"   allowfullscreen></iframe>
+            </div>
+            
+            <article>
+                <p>Spacial thanks to:</p>
+                <p>- CCEBA coordinators and jury: Emiliano Causa, Matias Romero Costas and Federico Joselevich</p>
+                <p>- Volcanic Ash: Andres Rey and Juan Rey</p>
+                <p>- Ironworks: Juan Manuel Toconas</p>
+                <p>- Model of the rock: Fabian Nonino</p>
+                <p>- Teaching of openGL and Shader Lenguajes: Fabricio Costa</p>
+                <p>- Logo and app icons Design: Jovana de Obaldia</P>
+                <p>- Photography and Video: Joaquin Aras, Agustin Anzorena y Tomas Rawski</p>
+            </article>
+
+            <div>
+                <a href="http://www.cceba.org.ar/"><img src="sponsor.jpg" alt="sponsor"/></a>
+                <a href="http://www.cceba.org.ar/"><img src="sponsor_2.jpg" alt="sponsor"/></a>
+            </div>
+
         </div>
-    </section>
         
 <?php include("../../footer.php"); ?>
