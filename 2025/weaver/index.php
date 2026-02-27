b019094ebe13976591fded50f3d478ff04052532
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-02-27 13:13:32 -0500

    invert order

--- 2025/weaver/index.php
+++ 2025/weaver/index.php
@@ -65,19 +65,18 @@
 
 
         <div id="longer-info">
-        <div>
-            <video controls width="100%" style="max-width: 800px;">
-            <source src="IMG_4879.MP4" type="video/mp4">
-            Your browser does not support the video tag.
-            </video>
-        </div>
-
-        <div>
-            <video controls width="100%" style="max-width: 800px;">
-            <source src="000.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-            </video>
-        </div>
+            <div>
+                <video controls width="100%" style="max-width: 800px;">
+                <source src="000.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+                </video>
+            </div>
+            <div>
+                <video controls width="100%" style="max-width: 800px;">
+                <source src="IMG_4879.MP4" type="video/mp4">
+                Your browser does not support the video tag.
+                </video>
+            </div>
         </div>
         <weaver-loader></weaver-loader>
         <overlay-controls></overlay-controls>
