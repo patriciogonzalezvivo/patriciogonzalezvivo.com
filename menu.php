f60f488ddf1ef945c4bb974bfc2360e4f3183ad4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-02-27 08:31:55 -0500

    clean menu

--- menu.php
+++ menu.php
@@ -6,16 +6,11 @@
 			<a id="menu_logo" href="/index.php"><img id="menu_logo_img" src="/images/menu_logo.png" alt="Tree Logo" /></a>
 			<nav id="menu_text">
 				<ul>
-					<li class="menu_item"><a href="/about.php"> Bio </a></li>
 					<li class="menu_item"><a href="/works.php"> Works </a></li>
+					<li class="menu_item"><a href="/about.php"> About </a></li>
+					<li class="menu_item"><a href="mailto:patriciogonzalezvivo@gmail.com"> Contact </a></li>
+					<li class="menu_item"><a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/instagram.png" width="32"/></a></li>
 				</ul>
 					
-				<section id="sidebar">
-					<a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank" target="_blank"><img src="/images/icons/instagram.png" width="32" class="icon" style="position: relative;left: 5px;"/></a>
-					<a href="https://twitter.com/intent/follow?screen_name=patriciogv" target="_blank"><img src="/images/icons/twitter.png" width="34" class="icon"/></a>
-					<a href="https://bsky.app/profile/patricio.io" target="_blank"><img src="/images/icons/bsky.png" width="24" class="icon" style="top: -5px;position: relative;"/></a>
-				</section >
 			</nav>
-		</div>	
-	';
-?>
+		</div>';?>
