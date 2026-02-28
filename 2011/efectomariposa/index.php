b29b12bb74ce48cbadb55707e04a6cf2014bc515
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-02-28 15:28:56 -0500

    EM

--- 2011/efectomariposa/index.php
+++ 2011/efectomariposa/index.php
@@ -2,36 +2,7 @@
 <?php include("../../menu.php");?>
 <?php include("../../sidebar.php");?>
 
-        <a href="https://vimeo.com/32321634" target="_blank">
-        <div id="myslides" class="roundPhoto" style="">
-            <img class="roundPhoto" src="puyehue-04.jpg" style="width: 100%; height: 640px; object-fit: cover;"/>
-            <img class="roundPhoto" src="01.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="02.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="03.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="04.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="05.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="06.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="07.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="08.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="09.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="10.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="11.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="12.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="13.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="14.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="15.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="16.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="17.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="18.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="19.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="20.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="21.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="22.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="23.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="24.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="25.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-            <img class="roundPhoto" src="26.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
-        </div></a>  
+        <script src="https://fast.wistia.com/player.js" async></script><script src="https://fast.wistia.com/embed/v2o6ybyv6g.js" async type="module"></script><style>wistia-player[media-id='v2o6ybyv6g']:not(:defined) { background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/v2o6ybyv6g/swatch'); display: block; filter: blur(5px); padding-top:56.25%; }</style> <wistia-player media-id="v2o6ybyv6g" aspect="1.7777777777777777"></wistia-player>
 
         <div id="longer-info">
             <article>
@@ -54,6 +25,37 @@
                     <iframe title="vimeo-player" src="https://player.vimeo.com/video/32321634?h=654b440c04" width="640" height="360" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"   allowfullscreen></iframe>
             </div>
             
+            <a href="https://vimeo.com/32321634" target="_blank">
+            <div id="myslides" class="roundPhoto" style="">
+                <img class="roundPhoto" src="puyehue-04.jpg" style="width: 100%; height: 640px; object-fit: cover;"/>
+                <img class="roundPhoto" src="01.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="02.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="03.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="04.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="05.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="06.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="07.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="08.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="09.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="10.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="11.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="12.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="13.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="14.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="15.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="16.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="17.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="18.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="19.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="20.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="21.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="22.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="23.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="24.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="25.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+                <img class="roundPhoto" src="26.jpg" style="width: 100%; height: 640px; object-fit: cover;" alt="slide"/>
+            </div></a>  
+
             <article>
                 <p>Spacial thanks to:</p>
                 <p>- CCEBA coordinators and jury: Emiliano Causa, Matias Romero Costas and Federico Joselevich</p>
