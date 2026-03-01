f940dbea345778457a6585983777d834ea4fd44c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-03-01 16:22:01 -0500

    communitas

--- 2010/communitas/README.md
+++ 2010/communitas/README.md
@@ -1,4 +1,1 @@
 > _“When we forget ourselves, we are the universe..”_ Hakui
-
-[**Communitas**](http://patriciogonzalezvivo.com/2010/communitas/) is an interactive table for collective drawings.
-
--- 2010/communitas/index.php
+++ 2010/communitas/index.php
@@ -3,8 +3,13 @@
 <?php include("../../menu.php");?>
 
 
-<iframe title="vimeo-player" src="https://player.vimeo.com/video/15171352?h=2b8e0fde57" width="640" height="360" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" allowfullscreen></iframe>
-	<!-- CONTENT -->
+	<!-- <iframe title="vimeo-player" src="https://player.vimeo.com/video/15171352?h=2b8e0fde57" width="640" height="360" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" allowfullscreen></iframe> -->
+	
+	
+	<script src="https://fast.wistia.com/player.js" async></script>
+	<script src="https://fast.wistia.com/embed/3sl00cbq2s.js" async type="module"></script><style>wistia-player[media-id='3sl00cbq2s']:not(:defined) { background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/3sl00cbq2s/swatch'); display: block; filter: blur(5px); padding-top:56.25%; }</style> 
+	<wistia-player media-id="3sl00cbq2s" aspect="1.7777777777777777"></wistia-player>
+	
 	<div id="longer-info">
 		<?php
 		include("../../parsedown/Parsedown.php");
