fe899d6776207b045c9772b815455baf2790e3ce
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-03-01 13:31:05 -0500

    selected

--- menu.php
+++ menu.php
@@ -6,8 +6,8 @@
 			<a id="menu_logo" href="/index.php"><img id="menu_logo_img" src="/images/menu_logo.png" alt="Tree Logo" /></a>
 			<nav id="menu_text">
 				<ul>
-					<li class="menu_item"><a href="/index.php"> Selected Works </a></li>
-					<li class="menu_item"><a href="/works.php"> Archive</a></li>
+					<li class="menu_item"><a href="/index.php"> Selected </a></li>
+					<li class="menu_item"><a href="/works.php"> Archive </a></li>
 					<li class="menu_item"><a href="/about.php"> About </a></li>
 					<li class="menu_item"><a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/instagram.png" width="32"/></a></li>
 				</ul>
