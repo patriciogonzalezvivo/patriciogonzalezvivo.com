7ddf4cf5e11f083941f9dcaaf8f739fedf3789a6
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-03-01 19:40:45 -0500

    related work

--- 2021/fen/index.php
+++ 2021/fen/index.php
@@ -29,6 +29,50 @@
             $Parsedown = new Parsedown();
             echo $Parsedown->text(file_get_contents('README.md'));
             ?>
+
+            <h2 style="text-align: center; margin: 2em 0 1em 0;">Related Works</h2>
+            <?php
+                $projects = [
+                    ['path' => '2023/blink'],
+                    ['path' => '2021/memory']
+                ];
+
+                foreach ($projects as $project) {
+                    $commented = isset($project['commented']) && $project['commented'];
+                    
+                    // Load metadata for projects with a path
+                    if (isset($project['path'])) {
+                        $meta = get_project_meta($project['path'], '../../');
+                        
+                        // Fix path to be relative from this page
+                        $meta['path'] = '../../' . $meta['path'];
+                        
+                        // Override with explicitly provided values
+                        if (isset($project['title'])) $meta['title'] = $project['title'];
+                        if (isset($project['year'])) $meta['year'] = $project['year'];
+                        if (isset($project['medium'])) $meta['medium'] = $project['medium'];
+                        if (isset($project['dimensions'])) $meta['dimensions'] = $project['dimensions'];
+                        if (isset($project['description'])) $meta['description'] = $project['description'];
+                        if (isset($project['url'])) $meta['url'] = $project['url'];
+                        if (isset($project['thumbnail'])) $meta['thumbnail'] = $project['thumbnail'];
+                    } else {
+                        // External project without local path - use provided metadata
+                        $meta = [
+                            'title' => $project['title'] ?? '',
+                            'year' => $project['year'] ?? '',
+                            'medium' => $project['medium'] ?? '',
+                            'dimensions' => $project['dimensions'] ?? '',
+                            'description' => $project['description'] ?? '',
+                            'url' => $project['url'],
+                            'thumbnail' => $project['thumbnail'] ?? '',
+                        ];
+                    }
+                    
+                    // Render the item
+                    echo render_project_item($meta, $commented);
+                }
+            ?>
+        
         </div>
 	
 <?php include("../../footer.php"); ?>
--- 2023/blink/index.php
+++ 2023/blink/index.php
@@ -42,6 +42,49 @@
             $Parsedown = new Parsedown();
             echo $Parsedown->text(file_get_contents('README.md'));
             ?>
+
+            <h2 style="text-align: center; margin: 2em 0 1em 0;">Related Works</h2>
+            <?php
+                $projects = [
+                    ['path' => '2021/memory'],
+                    ['path' => '2021/fen']
+                ];
+
+                foreach ($projects as $project) {
+                    $commented = isset($project['commented']) && $project['commented'];
+                    
+                    // Load metadata for projects with a path
+                    if (isset($project['path'])) {
+                        $meta = get_project_meta($project['path'], '../../');
+                        
+                        // Fix path to be relative from this page
+                        $meta['path'] = '../../' . $meta['path'];
+                        
+                        // Override with explicitly provided values
+                        if (isset($project['title'])) $meta['title'] = $project['title'];
+                        if (isset($project['year'])) $meta['year'] = $project['year'];
+                        if (isset($project['medium'])) $meta['medium'] = $project['medium'];
+                        if (isset($project['dimensions'])) $meta['dimensions'] = $project['dimensions'];
+                        if (isset($project['description'])) $meta['description'] = $project['description'];
+                        if (isset($project['url'])) $meta['url'] = $project['url'];
+                        if (isset($project['thumbnail'])) $meta['thumbnail'] = $project['thumbnail'];
+                    } else {
+                        // External project without local path - use provided metadata
+                        $meta = [
+                            'title' => $project['title'] ?? '',
+                            'year' => $project['year'] ?? '',
+                            'medium' => $project['medium'] ?? '',
+                            'dimensions' => $project['dimensions'] ?? '',
+                            'description' => $project['description'] ?? '',
+                            'url' => $project['url'],
+                            'thumbnail' => $project['thumbnail'] ?? '',
+                        ];
+                    }
+                    
+                    // Render the item
+                    echo render_project_item($meta, $commented);
+                }
+            ?>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/three@0.124/build/three.min.js"></script>
         <script type="module" src="main.min.js"></script>
