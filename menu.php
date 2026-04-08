e9fd2f6990ec67987f6be9dea736eecd40a2ec17
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-04-08 05:22:51 -0400

    responsibe

--- menu.php
+++ menu.php
@@ -1,19 +1,17 @@
 <?php
 	// main menu
 	echo '	
-
 		<div id="menu">
 			<div id="menu_header">
 				<a id="menu_logo" href="/index.php"><img id="menu_logo_img" src="/images/menu_logo.png" alt="Tree Logo" /></a>
-				<div id="menu_content">
-					<span id="menu_name">Patricio Gonzalez Vivo</span>
-					<nav id="menu_text">
-				<ul>
-					<li class="menu_item"><a href="/index.php"> Selected </a></li>
-					<li class="menu_item"><a href="/works.php"> Archive </a></li>
-					<li class="menu_item"><a href="/about.php"> About </a></li>
-					<li class="menu_item"><a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/instagram.png" width="32"/></a></li>
-				</ul>
-					</nav>
-				</div>
-			</div>';?>
+				<span id="menu_name">PATRICIO GONZALEZ VIVO</span>
+				<nav id="menu_text">
+					<ul>
+						<li class="menu_item"><a href="/index.php"> Selected </a></li>
+						<li class="menu_item"><a href="/works.php"> Archive </a></li>
+						<li class="menu_item"><a href="/about.php"> About </a></li>
+						<li class="menu_item"><a href="http://www.instagram.com/patriciogonzalezvivo" target="_blank"><img src="/images/icons/instagram.png" width="32"/></a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>';?>
