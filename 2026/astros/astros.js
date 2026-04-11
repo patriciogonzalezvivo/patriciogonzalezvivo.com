cdf5d2bd41b4a0c6c8e2e17fa15f13c8982b7cb4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2026-04-11 11:19:55 -0400

    latest astros

--- 2026/astros/astros.js
+++ 2026/astros/astros.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpcn40rx2l.js
+// include: /tmp/tmpj18v3jvb.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -202,25 +202,25 @@ Module['FS_createPath']("/", "ttf", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/png/gold_matcap.jpg", "start": 0, "end": 82753}, {"filename": "/png/noise_blue.png", "start": 82753, "end": 473790}, {"filename": "/png/silver_matcap.jpg", "start": 473790, "end": 535230}, {"filename": "/shaders/flood_decoding.frag", "start": 535230, "end": 535656}, {"filename": "/shaders/matcap_font.frag", "start": 535656, "end": 537879}, {"filename": "/shaders/matcap_font.vert", "start": 537879, "end": 538606}, {"filename": "/shaders/matcap_lines.frag", "start": 538606, "end": 539388}, {"filename": "/shaders/matcap_lines.vert", "start": 539388, "end": 539806}, {"filename": "/shaders/postprocessing.frag", "start": 539806, "end": 541052}, {"filename": "/shaders/raymarching.frag", "start": 541052, "end": 544665}, {"filename": "/shaders/stars.frag", "start": 544665, "end": 545172}, {"filename": "/shaders/stars.vert", "start": 545172, "end": 545512}, {"filename": "/ttf/astronomicon.ttf", "start": 545512, "end": 577076}], "remote_package_size": 577076});
+    loadPackage({"files": [{"filename": "/png/gold_matcap_001.jpg", "start": 0, "end": 75605}, {"filename": "/png/noise_blue.png", "start": 75605, "end": 466642}, {"filename": "/shaders/background.frag", "start": 466642, "end": 468559}, {"filename": "/shaders/flood_decoding.frag", "start": 468559, "end": 468985}, {"filename": "/shaders/matcap_font.frag", "start": 468985, "end": 471208}, {"filename": "/shaders/matcap_font.vert", "start": 471208, "end": 471935}, {"filename": "/shaders/matcap_lines.frag", "start": 471935, "end": 473201}, {"filename": "/shaders/matcap_lines.vert", "start": 473201, "end": 473619}, {"filename": "/shaders/postprocessing.frag", "start": 473619, "end": 474865}, {"filename": "/shaders/raymarching.frag", "start": 474865, "end": 478478}, {"filename": "/shaders/stars.frag", "start": 478478, "end": 478985}, {"filename": "/shaders/stars.vert", "start": 478985, "end": 479325}, {"filename": "/ttf/astronomicon.ttf", "start": 479325, "end": 510889}], "remote_package_size": 510889});
 
   })();
 
-// end include: /tmp/tmpcn40rx2l.js
-// include: /tmp/tmp907sh2gg.js
+// end include: /tmp/tmpj18v3jvb.js
+// include: /tmp/tmp_0wtnlna.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp907sh2gg.js
-// include: /tmp/tmp8abookwv.js
+  // end include: /tmp/tmp_0wtnlna.js
+// include: /tmp/tmp27pdm661.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp8abookwv.js
+  // end include: /tmp/tmp27pdm661.js
 
 
 var arguments_ = [];
