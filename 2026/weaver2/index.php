495fe4f47388e38681f7c0e6ecbe7eecb795134b
Patricio Gonzalez Vivo <patriciog@adobe.com>
2026-06-07 19:40:12 -0400

    project

--- 2026/weaver2/index.php
+++ 2026/weaver2/index.php
@@ -39,14 +39,15 @@
                 echo $Parsedown->text(file_get_contents('README.md'));
             ?>
 
-            <?php include("../../server/slideSet.php"); echo slideset('images', 'width: 100%;'); ?>
-
             <div>
                 <video controls width="100%" style="max-width: 800px;">
                 <source src="000.mp4" type="video/mp4">
                 Your browser does not support the video tag.
                 </video>
             </div>
+
+            <?php include("../../server/slideSet.php"); echo slideset('images', 'width: 100%;'); ?>
+
 
             <h2>Related Works</h2>
             
